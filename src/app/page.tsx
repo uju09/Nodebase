98d53f1dbcf6a2055f4ea9eb95f2c30a00f64d4d
uju09 <24bcs012@iiitdwd.ac.in>
2026-01-07 16:51:17 +0530

    chore: setup tRPC client and server
    Signed-off-by: uju09 <24bcs012@iiitdwd.ac.in>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { prisma } from '../lib/prisma'
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
-const Home = async () => {
-  const user = await prisma.user.findMany();
+const Home = () => {
+  const trpc = useTRPC();
+  const { data: user } = useQuery(trpc.getUsers.queryOptions());
   return (
     <div className="flex justify-center items-center">
       <h1 className="text-amber-400">Home</h1>
